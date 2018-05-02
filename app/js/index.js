df1b24124093469a6d6839497eaafac6bf54b6c5
WuChenDi <996194720@qq.com>
2018-05-02 22:51:14 +0800

    // 解构赋值--数组解构赋值

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,2 @@
-import './class/lesson1';
+// import './class/lesson1';
+import './class/lesson2';
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -86,36 +86,18 @@ __webpack_require__(2);
 "use strict";
 
 
-// let const
+// 解构赋值
+// 数组解构赋值   对象解构赋值   字符串解构赋值
+// 布尔值解构赋值   函数参数解构赋值   数值解构赋值
+{
+  var a = void 0,
+      b = void 0,
+      rest = void 0;
+  a = 1;
+  b = 2;
 
-function test() {}
-// let 块级作用域
-// for (let i = 1; i < 3; i++) {
-//   console.log(i);
-// }
-// console.log(i);
-
-// let不允许在相同作用域内重复声明同一个变量，即同一个作用域内不允许出现名称相同的变量。
-// let a = 1;
-// let a = 2;
-
-
-// test();
-
-function last() {
-  // const用于声明常量，一旦声明，必须立即赋值，且以后不可更改。
-  var PI = 3.1415926;
-  // const PI;
-  // PI = 8;
-  // 这里的k为对象的时候，对象是引用类型，这里的返回值是对象的指针，k是指向这个对象存储的这个指针，这个指针是不变的，但是，对象本身是可以变的
-  var k = {
-    a: 1
-  };
-  k.b = 3;
-  console.log(PI, k);
+  console.log(a, b);
 }
-
-last();
 
 /***/ })
 /******/ ]);
