38af3afce43de049edacc75b0357bad3e1763c18
WuChenDi <996194720@qq.com>
2018-05-03 18:54:12 +0800

    padStart padEnd

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -9695,5 +9695,11 @@ module.exports = function (regExp, replace) {
   console.log(m);
 }
 
+{
+  // ES2017 引入了字符串补全长度的功能。如果某个字符串不够指定长度，会在头部或尾部补全。padStart()用于头部补全，padEnd()用于尾部补全。
+  console.log('1'.padStart(2, '0'));
+  console.log('1'.padEnd(2, '0'));
+}
+
 /***/ })
 /******/ ]);
