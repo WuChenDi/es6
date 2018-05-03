82d8d760d5917a87b3e30985b714d3a0311f3aed
WuChenDi <996194720@qq.com>
2018-05-03 16:35:04 +0800

    ₻

--- app/js/class/lesson4.js
+++ app/js/class/lesson4.js
@@ -1,1 +1,6 @@
 // 字符串扩展
+
+{
+  console.log('a','\u0061');
+  console.log('s','\u20BB7');
+}
--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,6 @@
 // import './class/lesson1';
 // import './class/lesson2';
 // import './class/lesson3';
+
+import 'babel-polyfill';
 import './class/lesson4';
