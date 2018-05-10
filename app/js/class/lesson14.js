81238b6bd35154c5a2a43f9bb45ffb37fbaa07fb
WuChenDi <996194720@qq.com>
2018-05-10 11:00:36 +0800

    Symbol.iterator

--- app/js/class/lesson14.js
+++ app/js/class/lesson14.js
@@ -4,9 +4,40 @@
 // for...of
 
 {
-  let arr = ['hello','world'];
+  let arr = ['hello', 'world'];
   let map = arr[Symbol.iterator]();
   console.log(map.next());
   console.log(map.next());
   console.log(map.next());
+}
+
+{
+  let obj = {
+    seart: [1, 3, 2],
+    end: [7, 9, 8],
+    [Symbol.iterator]() {
+      let self = this;
+      let index = 0;
+      let arr = self.seart.concat(self.end);
+      let len = arr.length;
+      return {
+        next() {
+          if (index < len) {
+            return {
+              value: arr[index++],
+              done: false
+            }
+          } else {
+            return {
+              value: arr[index++],
+              done: true
+            }
+          }
+        }
+      }
+    }
+  }
+  for (let key of obj) {
+    console.log(key);
+  }
 }
--- server/public/js/index.js
+++ server/public/js/index.js
@@ -4008,6 +4008,8 @@ __webpack_require__(128);
 "use strict";
 
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 // Iterator和for...of循环
 // 什么是Iterator接口
 // Iterator的基本用法
@@ -4019,6 +4021,57 @@ __webpack_require__(128);
   console.log(map.next());
   console.log(map.next());
   console.log(map.next());
+}
+
+{
+  var obj = _defineProperty({
+    seart: [1, 3, 2],
+    end: [7, 9, 8]
+  }, Symbol.iterator, function () {
+    var self = this;
+    var index = 0;
+    var arr = self.seart.concat(self.end);
+    var len = arr.length;
+    return {
+      next: function next() {
+        if (index < len) {
+          return {
+            value: arr[index++],
+            done: false
+          };
+        } else {
+          return {
+            value: arr[index++],
+            done: true
+          };
+        }
+      }
+    };
+  });
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = obj[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var key = _step.value;
+
+      console.log(key);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
 }
 
 /***/ }),
