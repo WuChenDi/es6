dd1730b64d3971a1c60910601e4ee785b86c137d
WuChenDi <996194720@qq.com>
2018-05-10 15:34:32 +0800

    计算模块

--- server/public/js/index.js
+++ server/public/js/index.js
@@ -9643,6 +9643,130 @@ throw new Error("Module build failed: SyntaxError: D:/GitHub/es6/app/js/lottery/
 "use strict";
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Calculate = function () {
+  function Calculate() {
+    _classCallCheck(this, Calculate);
+  }
+
+  _createClass(Calculate, [{
+    key: 'computeCount',
+
+    /**
+     * [computeCount 计算注数]
+     * @param  {number} active    [当前选中的号码]
+     * @param  {string} play_name [当前的玩法标识]
+     * @return {number}           [注数]
+     */
+    value: function computeCount(active, paly_name) {
+      var count = 0;
+      var exist = this.play_list.has(paly_name);
+      var arr = new Array(active).fill('0');
+      if (exist && paly_name.at(0) === 'r') {
+        count = Calculate.combine(arr, play_name.split('')[1]).length;
+      }
+      return count;
+    }
+
+    /**
+     * [computeBonus 奖金范围预测]
+     * @param  {number} active    [当前选中的号码]
+     * @param  {string} play_name [当前的玩法标识]
+     * @return {array}            [奖金范围]
+     */
+
+  }, {
+    key: 'computeBonus',
+    value: function computeBonus(active, paly_name) {
+      var play = play_name.split('');
+      var self = this;
+      var arr = new Array(play[1] * 1).fill(0);
+      var min = void 0,
+          max = void 0;
+      if (play[0] === 'r') {
+        var min_active = 5 - (11 - active);
+        if (min_active > 0) {
+          if (min_active - play[1] >= 0) {
+            arr = new Array(min_active).fill(0);
+            min = Calculate.combine(arr, play[1]).length;
+          } else {
+            if (play[i] - 5 > 0 && active - play[1] >= 0) {
+              arr = new Array(active - 5).fill(0);
+              min = Calculate.combine(arr, play[1] - 5).length;
+            } else {
+              min = active - play[1] > -1 ? 1 : 0;
+            }
+          }
+        } else {
+          min = active - play[1] > -1 ? 1 : 0;
+        }
+        var max_active = Math.min(active, 5);
+        if (play[1] - 5 > 0) {
+          if (active - play[1] >= 0) {
+            arr = new Array(active - 5).fill(0);
+            max = Calculate.combine(arr, play[1] - 5).length;
+          } else {
+            max = 0;
+          }
+        } else if (paly[1] - 5 < 0) {
+          arr = new Array(Math.min(active, 5)).fill(0);
+          max = Calculate.combine(arr, play[1].length);
+        } else {
+          max = 1;
+        }
+      }
+      return [min, max].map(function (item) {
+        return item * self.play_list.get(play_name).bonus;
+      });
+    }
+
+    /**
+     * [combine 组合运算]
+     * @param  {array} arr  [参与组合运算的数组]
+     * @param  {number} size [组合运算的基数]
+     * @return {number}      [计算注数]
+     */
+
+  }], [{
+    key: 'combine',
+    value: function combine(arr, size) {
+      var allResult = [];
+      (function f(arr, size, result) {
+        var arrLen = arr.length;
+        if (size > arrLen) {
+          return;
+        }
+        if (size === arrLen) {
+          allResult.push([].concat(result, arr));
+        } else {
+          for (var _i = 0; _i < arrLen; _i++) {
+            var newResult = [].concat(result);
+            newResult.push(arr[_i]);
+            if (size === 1) {
+              allResult.push(newResult);
+            } else {
+              var newArr = [].concat(arr);
+              newArr.splice(0, _i + 1);
+              f(newArr, size - 1, newResult);
+            }
+          }
+        }
+      })(arr, size, []);
+    }
+  }]);
+
+  return Calculate;
+}();
+
+exports.default = Calculate;
+
 /***/ }),
 /* 334 */
 /***/ (function(module, exports, __webpack_require__) {
